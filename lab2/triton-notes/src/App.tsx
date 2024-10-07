9142dae1e20d8aeb6d1b8285db7b40aee0a8dda3
Meron19 <meronamershaoo@gmail.com>
2024-10-07 14:52:57 -0700

    Exercise 4.2 done

--- lab2/triton-notes/src/App.tsx
+++ lab2/triton-notes/src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
 import { Label, Note } from "./types"; // Import the Label type from the appropriate module
 import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
+import { useState, useEffect } from "react"; // Import the useState and useEffect hooks
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>(dummyNotesList); // Initialize the notes state with the dummyNotesList
+  const [favorites, setFavorites] = useState<string[]>([]); // Initialize the favorites state with an empty array
+
+  useEffect(() => {
+    console.log("favorites: ", favorites);
+  }, [favorites]);
+
+  const handleLike = (noteId: number) => {
+    const newFavorites = favorites.includes(noteId.toString())
+      ? favorites.filter((id) => id !== noteId.toString())
+      : [...favorites, noteId.toString()];
+
+    setFavorites(newFavorites);
+  };
+
   return (
     <div className="app-container">
       <form className="note-form">
@@ -11,18 +27,29 @@ function App() {
         </div>
 
         <div>
-          <textarea></textarea>
+          <textarea placeholder="Note Content"></textarea>
         </div>
+
+        <select id="label" name="label">
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+          <option value="study">Study</option>
+          <option value="other">Other</option>
+        </select> 
 
         <div>
           <button type="submit">Create Note</button>
         </div>
       </form>
       <div className="notes-grid">
-        {dummyNotesList.map((note) => (
+        {notes.map((note) => (
           <div key={note.id} className="note-item">
             <div className="notes-header">
-              <button>x</button>
+              <button
+                onClick={() => handleLike(note.id)}
+              >
+                {favorites.includes(note.id.toString()) ? <span>&#9829;</span> : <span>&#9825;</span>}
+              </button>
             </div>
             <h2> {note.title} </h2>
             <p> {note.content} </p>
@@ -30,8 +57,17 @@ function App() {
           </div>
         ))}
       </div>
+      <div className="favorites">
+        <h2>List of favorites:</h2>
+        <ul>
+          {favorites.map((noteId) => (
+            <li key={noteId}>{notes.find((note) => note.id === +noteId)?.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
