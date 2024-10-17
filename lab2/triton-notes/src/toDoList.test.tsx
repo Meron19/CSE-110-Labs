9044aad5ed613fce34ab9e154a27c38855bfcf35
Meron19 <meronamershaoo@gmail.com>
2024-10-16 21:06:33 -0700

    toDoList unit test done!

--- lab2/triton-notes/src/toDoList.test.tsx
+++ lab2/triton-notes/src/toDoList.test.tsx
@@ -1,58 +1,55 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { ToDoList } from "./toDoList";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 describe("Read ToDoList", () => {
-  test("Read ToDoList", () => {
-    render(<ToDoList />);
+    test("renders ToDoList items", () => {
+        render(
+            <BrowserRouter>
+              <ToDoList />
+            </BrowserRouter>
+          );
 
-    const toDo1 = screen.getByText("Apples");
-    const toDo2 = screen.getByText("Bananas");
+        const toDo1 = screen.getByText("Apples");
+        const toDo2 = screen.getByText("Bananas");
+    
+        expect(toDo1).toBeInTheDocument();
+        expect(toDo2).toBeInTheDocument();
+      });
+    
+      test("renders checkboxes for ToDoList items", () => {
+        render(
+            <BrowserRouter>
+              <ToDoList />
+            </BrowserRouter>
+          );
+        //check if they appear
+        const toDo1Checkbox = screen.getByText("Apples");
+        const toDo2Checkbox = screen.getByText("Bananas");
+    
+        expect(toDo1Checkbox).toBeInTheDocument();
+        expect(toDo2Checkbox).toBeInTheDocument();
+      });
 
-    expect(toDo1).toBeInTheDocument();
-    expect(toDo2).toBeInTheDocument();
-  });
-});
+      test("number of items checked the same as shown in the title",() =>{
+        render(<BrowserRouter><ToDoList /></BrowserRouter>);
 
-describe("Read ToDoList", () => {
-  test("Read ToDoList", () => {
-    render(<ToDoList />);
+          expect(screen.getByText("Items bought: 0")).toBeInTheDocument();
+        //get the checkboxes
+          const check = screen.getAllByRole("checkbox");
 
-    const toDo1Checkbox = screen.getByRole("checkbox", { name: "Apples" });
-    const toDo2Checkbox = screen.getByRole("checkbox", { name: "Bananas" });
-
-    expect(toDo1Checkbox).toBeInTheDocument();
-    expect(toDo2Checkbox).toBeInTheDocument();
-  });
-});
-
-describe("ToDoList total counter", () => {
-    test("Should display the correct count of purchased items", () => {
-      render(
-        <BrowserRouter>
-          <ToDoList />
-        </BrowserRouter>
-      );
-  
-      
-      const title = screen.getByText("Items bought: 0");
-      expect(title).toBeInTheDocument();
-  
-
-      const toDoCheckbox = screen.getAllByRole("checkbox");
-  
-
-      fireEvent.click(toDoCheckbox[0]);
-  
-
-      const item1 = screen.getByText("Items bought: 1");
-      expect(item1).toBeInTheDocument();
-  
-
-      fireEvent.click(toDoCheckbox[0]);
-  
-
-      const item2 = screen.getByText("Items bought: 0");
-      expect(item2).toBeInTheDocument();
+        //test if you can click the unchecked and items increment
+          const unchecked = check[0]
+          fireEvent.click(unchecked);
+          expect(screen.getByText("Items bought: 1")).toBeInTheDocument();
+          fireEvent.click(unchecked);
+          expect(screen.getByText("Items bought: 2")).toBeInTheDocument();
+        
+          //test if you can click the checked and items decrement
+          const checked = check[1]
+          fireEvent.click(checked);
+          expect(screen.getByText("Items bought: 1")).toBeInTheDocument();
+          fireEvent.click(checked);
+          expect(screen.getByText("Items bought: 0")).toBeInTheDocument();
+      });
     });
-  });
