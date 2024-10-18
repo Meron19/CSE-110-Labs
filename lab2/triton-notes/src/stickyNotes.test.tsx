5e52032da7dfccf195b014e88c6fb60aedc0e2a8
jel084 <130106981+jel084@users.noreply.github.com>
2024-10-17 19:22:31 -0700

    added edge case to stickynotes

--- lab2/triton-notes/src/stickyNotes.test.tsx
+++ lab2/triton-notes/src/stickyNotes.test.tsx
@@ -135,3 +135,59 @@ describe("Read Sticky Notes", () => {
              expect(note6content).toBeInTheDocument();
          });  
 });
+
+describe("delete ALL sticky notes", () => {
+  test("Delete sticky note dummy 3,4,5,6", () => {
+    render(<StickyNotes />);
+
+    const note1 = screen.getByText("test note 1 title");
+    const note1content = screen.getByText("test note 1 content");
+
+    const note2 = screen.getByText("test note 2 title");
+    const note2content = screen.getByText("test note 2 content");
+
+    const note3 = screen.getByText("test note 3 title");
+    const note3content = screen.getByText("test note 3 content");
+
+    const note4 = screen.getByText("test note 4 title");
+    const note4content = screen.getByText("test note 4 content");
+
+    const note5 = screen.getByText("test note 5 title");
+    const note5content = screen.getByText("test note 5 content");
+
+    const note6 = screen.getByText("test note 6 title");
+    const note6content = screen.getByText("test note 6 content");
+
+    const deleteButton1 = screen.getAllByText("X")[0];
+    const deleteButton2 = screen.getAllByText("X")[1];
+    const deleteButton3 = screen.getAllByText("X")[2];
+    const deleteButton4 = screen.getAllByText("X")[3];
+    const deleteButton5 = screen.getAllByText("X")[4];
+    const deleteButton6 = screen.getAllByText("X")[5];
+
+    fireEvent.click(deleteButton1);
+    fireEvent.click(deleteButton2);
+    fireEvent.click(deleteButton3);
+    fireEvent.click(deleteButton4);
+    fireEvent.click(deleteButton5);
+    fireEvent.click(deleteButton6);
+
+    expect(note1).not.toBeInTheDocument();
+    expect(note1content).not.toBeInTheDocument();
+
+    expect(note2).not.toBeInTheDocument();
+    expect(note2content).not.toBeInTheDocument();
+
+    expect(note3).not.toBeInTheDocument();
+    expect(note3content).not.toBeInTheDocument();
+
+    expect(note4).not.toBeInTheDocument();
+    expect(note4content).not.toBeInTheDocument();
+
+    expect(note5).not.toBeInTheDocument();
+    expect(note5content).not.toBeInTheDocument();
+
+    expect(note6).not.toBeInTheDocument();
+    expect(note6content).not.toBeInTheDocument();
+  });
+});
